68caf5c5e114ef66e9e80f9c2bc94b9ae811fcfe
Dilandoolabh <141252188+Dilandoolabh@users.noreply.github.com>
2025-09-24 18:26:35 +0000

    second video

--- projects/tools-app/src/components/parent_child/parent_child_home.rs
+++ projects/tools-app/src/components/parent_child/parent_child_home.rs
@@ -1,12 +1,20 @@
 use leptos::*;
-
+use leptos_router::*;
 
 #[component]
 pub fn ParentChildHome() -> impl IntoView {
     view! {
         <div>
             <h2>"Parent Child Home"</h2>
-            <p>"This is the Parent Child Home page."</p>
+            <ul>
+                <li>
+                    <a href="/parent-child/write-signal">"Write Signal"</a>
+                </li>
+                <li>
+                    <a href="/parent-child/callback">"Callback"</a>
+                </li>
+            </ul>
+            <Outlet />
         </div>
     }
 }
