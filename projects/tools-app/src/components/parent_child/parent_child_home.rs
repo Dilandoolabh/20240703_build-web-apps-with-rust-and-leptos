30acd1fa08ff5d2da5ad602311b64c02e6261753
Dilandoolabh <141252188+Dilandoolabh@users.noreply.github.com>
2025-09-24 11:21:17 +0000

    first video

---
+++ projects/tools-app/src/components/parent_child/parent_child_home.rs
@@ -0,0 +1,12 @@
+use leptos::*;
+
+
+#[component]
+pub fn ParentChildHome() -> impl IntoView {
+    view! {
+        <div>
+            <h2>"Parent Child Home"</h2>
+            <p>"This is the Parent Child Home page."</p>
+        </div>
+    }
+}
